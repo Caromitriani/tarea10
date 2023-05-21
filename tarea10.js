18664e0a892c5d0675c0c483e2bb9d65d65b7f97
Caromitriani <130710180+Caromitriani@users.noreply.github.com>
2023-05-21 02:08:25 -0300

    Add files via upload

---
+++ tarea10.js
@@ -0,0 +1,15 @@
+var nombre = "Carolina";
+var apellido = "Mitriani";
+console.log(nombre + apellido);
+
+var numero1 = 12;
+var numero2 = 24;
+var suma = numero1 + numero2;
+console.log(suma);
+
+var numero1 = 12;
+var numero2 = 24;
+var suma = numero1 + numero2;
+var numeroExtra = suma + 10;
+
+console.log(numeroExtra);
